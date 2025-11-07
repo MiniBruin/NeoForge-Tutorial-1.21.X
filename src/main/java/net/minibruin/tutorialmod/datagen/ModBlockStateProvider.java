7939c173af53b88a90d3e649ffefbc94c7ecaee4
MiniBruin <debruinjorn@gmail.com>
2025-11-07 17:41:11 +0100

    Datagen files for easy json creating made

---
+++ src/main/java/net/minibruin/tutorialmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,28 @@
+package net.minibruin.tutorialmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minibruin.tutorialmod.TutorialMod;
+import net.minibruin.tutorialmod.block.ModBlocks;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TutorialMod.MOD_ID,exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BISMUTH_BLOCK);
+
+        blockWithItem(ModBlocks.BISMUTH_ORE);
+        blockWithItem(ModBlocks.BISMUTH_DEEPSLATE_ORE);
+
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+    }
+
+    private void blockWithItem(DeferredBlock<?> deferredBlock){
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
+}
